12d2ba2f5e7f111af6a9902160997369e006ee0b
Kasun <kmadushanka40@gmail.com>
2017-06-28 20:46:17 +0530

    data insert

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ require 'db.php';
 $username=$_GET["username"];
 $password=$_GET["password"];
 
+echo $username;
+
 $sql="INSERT INTO user (firstName, lastName, email,password) VALUES ('admin', 'admin','$username','$password')";
    if (mysqli_query($con,$sql)) {
       echo "Values have been inserted successfully";
