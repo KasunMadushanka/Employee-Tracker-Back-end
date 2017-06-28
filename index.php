d70dba473d5092c3c5d3b161761d94c5e4c93dc8
Kasun <kmadushanka40@gmail.com>
2017-06-28 23:57:50 +0530

    data insert

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php
 require 'db.php';
 
-$username=$_POST["username"];
+//$username=$_POST["username"];
 //$password=$_GET["password"];
 
 //echo $username;
 
-$sql="INSERT INTO user (firstName, lastName, email,password) VALUES ('"+$username+"', 'admin','4g','4g')";
+$sql="INSERT INTO user (firstName, lastName, email,password) VALUES ('B', 'admin','4g','4g')";
    if (mysqli_query($con,$sql)) {
       echo "Values have been inserted successfully";
    }
