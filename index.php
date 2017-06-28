455213fbec56b70c620bbdd3a0746995d3c2a0c3
Kasun <kmadushanka40@gmail.com>
2017-06-28 20:40:55 +0530

    data insert

--- index.php
+++ index.php
@@ -1,35 +1,11 @@
 <?php
+require 'db.php';
 
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
+$username=$_GET["username"];
+$password=$_GET["password"];
 
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
-$link = mysqli_connect($connectstr_dbhost, $connectstr_dbusername, $connectstr_dbpassword,$connectstr_dbname);
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-echo "host" .$connectstr_dbhost;
-echo "username ".$connectstr_dbusername;
-echo "password ".$connectstr_dbpassword;
-
-mysqli_close($link);
+$sql="INSERT INTO user (firstName, lastName, email,password) VALUES ('admin', 'admin','$username','$password')";
+   if (mysqli_query($con,$sql)) {
+      echo "Values have been inserted successfully";
+   }
 ?>
