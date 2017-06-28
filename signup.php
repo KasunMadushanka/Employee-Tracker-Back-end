2766247b69cc362a6b0783ab4d57b3c041976f54
Kasun <kmadushanka40@gmail.com>
2017-06-28 23:11:37 +0530

    data insert

---
+++ signup.php
@@ -0,0 +1,14 @@
+<?php
+require 'db.php';
+
+$firstName='ii';//$_POST['firstName'];
+$lastName='y'//$_POST['lastName'];
+$username=$_POST["username"];
+$password='g';//$_GET["password"];
+
+$sql="INSERT INTO user (firstName, lastName, email,password) VALUES ("$firstName","$lastName","$username","$password")";
+if (mysqli_query($con,$sql)) {
+    echo "Values have been inserted successfully";
+}
+
+?>
