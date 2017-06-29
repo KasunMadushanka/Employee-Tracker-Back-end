0a414387a629dbb4d302d47f446526e84f001fe2
Kasun <kmadushanka40@gmail.com>
2017-06-29 09:51:57 +0530

    data insert

--- login.php
+++ login.php
@@ -1,15 +1,18 @@
 <?php
 require 'db.php';
 
-$username=$_GET["username"];
-$password=$_GET["password"];
+//$username=$_GET["username"];
+//$password=$_GET["password"];
 
-$sql="SELECT* FROM user where username=$username and password=$password";
+$sql="SELECT* FROM user";
 $result=mysqli_query($con,$sql);
 if (mysqli_num_rows($result)>0) {
-    echo 1;
+    while($row = mysqli_fetch_assoc($result)) {
+       echo "kasun";
+   }
+    //echo 1;
 }else{
-    echo 0;
+    //echo 0;
 }
 
 ?>
