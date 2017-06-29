3fb5d3adc187290c8d7660ec4cee1fc8b38ab24c
Kasun <kmadushanka40@gmail.com>
2017-06-29 09:55:03 +0530

    data insert

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $sql="SELECT* FROM user";
 $result=mysqli_query($con,$sql);
 if (mysqli_num_rows($result)>0) {
     while($row = mysqli_fetch_assoc($result)) {
-       echo "kasun\n";
+       echo $row["firstName"];
    }
     //echo 1;
 }else{
