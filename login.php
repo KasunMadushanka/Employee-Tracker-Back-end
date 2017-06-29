66252f2287032ca6dea7904923bbfba9a6611782
Kasun <kmadushanka40@gmail.com>
2017-06-29 10:48:24 +0530

    data insert

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require 'db.php';
 $username=urldecode($_POST['username']);
 $username=urldecode($_POST['password']);
 
-$sql="SELECT* FROM user where username='$username' and password='$password'";
+$sql="SELECT* FROM user where username=$username and password=$password";
 $result=mysqli_query($con,$sql);
 if (mysqli_num_rows($result)>0) {
     echo 1;
