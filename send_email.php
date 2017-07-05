a9cd82bc5edfe2230c9500bfbba61feb30696061
Kasun <kmadushanka40@gmail.com>
2017-07-05 23:00:55 +0530

    email function added

--- send_email.php
+++ send_email.php
@@ -3,7 +3,7 @@
 //$tourId = $_POST['tourId'];
 
 $from_name = "Employee Tracker";
-$from_mail = "emptracker.com";
+$from_mail = "kasunm07@yahoo.com";
 $replyto = "emptracker.com";
 $mailto =  . "kmadushanka40@gmail.com";
 
@@ -17,13 +17,13 @@ $message = "Hello";
 //$file = $path . $filename;
 //$content = file_get_contents($file);
 //$content = chunk_split(base64_encode($content));
-$uid = md5(uniqid(time()));
+//$uid = md5(uniqid(time()));
 //$name = basename($file);
 
 // header
 $header = "From: " . $from_name . " <" . $from_mail . ">\r\n";
 $header .= "Reply-To: " . $replyto . "\r\n";
-$headers.= "cc: " . "Kasun Madushanka" . " <" . "emptracker.com" . ">" . "\r\n";
+$headers.= "cc: " . "Kasun Madushanka" . " <" . "kasunm07@yahoo.com" . ">" . "\r\n";
 $header .= "MIME-Version: 1.0\r\n";
 //$header .= "Content-Type: multipart/mixed; boundary=\"" . $uid . "\"\r\n\r\n";
 
@@ -32,12 +32,12 @@ $nmessage = "--" . $uid . "\r\n";
 $nmessage .= "Content-type:text/plain; charset=iso-8859-1\r\n";
 $nmessage .= "Content-Transfer-Encoding: 7bit\r\n\r\n";
 $nmessage .= $message . "\r\n\r\n";
-$nmessage .= "--" . $uid . "\r\n";
+//$nmessage .= "--" . $uid . "\r\n";
 //$nmessage .= "Content-Type: application/octet-stream; name=\"" . $filename . "\"\r\n";
 //$nmessage .= "Content-Transfer-Encoding: base64\r\n";
 //$nmessage .= "Content-Disposition: attachment; filename=\"" . $filename . "\"\r\n\r\n";
 //$nmessage .= $content . "\r\n\r\n";
-//$nmessage .= "--" . $uid . "--";
+$nmessage .= "--" . $uid . "--";
 
 mail($mailto, $subject, $nmessage, $header);
 ?>
